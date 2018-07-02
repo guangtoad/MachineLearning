32da93ba45cbc534a974946467638ad2d7f78815
guangtoad <guang_toad@hotmail.com>
2018-07-02 18:02:02 +0800

    0.0.0

---
+++ MachineLearning_ios/MachineLearning_iosTests/MachineLearning_iosTests.m
@@ -0,0 +1,39 @@
+//
+//  MachineLearning_iosTests.m
+//  MachineLearning_iosTests
+//
+//  Created by home on 2018/6/28.
+//  Copyright © 2018年 toad. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MachineLearning_iosTests : XCTestCase
+
+@end
+
+@implementation MachineLearning_iosTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
